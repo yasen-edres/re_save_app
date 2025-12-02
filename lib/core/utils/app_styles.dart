9a842015033228cbcde8761cc6c8d18091dfb17c
yassin <yase.ehab.230993@gmail.com>
2025-12-02 23:10:51 +0200

    + create login screen

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'app_colors.dart';
+
+class AppStyles {
+  static TextStyle regular12Text = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryDark,
+  );
+  static TextStyle regular11SalePrice = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryDark,
+  );
+  static TextStyle regular14Text = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryDark,
+  );
+  static TextStyle regular18White = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  static TextStyle light14SearchHint = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    color: AppColors.searchHintColor,
+  );
+  static TextStyle bold16Green = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: AppColors.greenColor,
+  );
+  static TextStyle bold16Primary = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryColor,
+  );
+  static TextStyle light16White = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    color: AppColors.whiteColor,
+  );
+  static TextStyle light18HintText = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w300,
+    color: AppColors.hintTextColor,
+  );
+  static TextStyle semi16TextWhite = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.whiteColor,
+  );
+  static TextStyle semi20Primary = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryColor,
+  );
+  static TextStyle semi24White = GoogleFonts.poppins(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: AppColors.whiteColor,
+  );
+  static TextStyle medium14Category = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primaryDark,
+  );
+  static TextStyle medium14LightPrimary = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primaryDarkLight,
+  );
+  static TextStyle medium14PrimaryDark = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primaryDark,
+  );
+  static TextStyle medium18Header = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primaryDark,
+  );
+  static TextStyle medium18White = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: AppColors.whiteColor,
+  );
+  static TextStyle medium20White = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppColors.whiteColor,
+  );
+}

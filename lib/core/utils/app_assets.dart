e1f66175f04859bf0647bb69ee798a4f61c2cdb5
yassin <yase.ehab.230993@gmail.com>
2025-12-06 15:22:16 +0200

    + create category and events

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -2,4 +2,21 @@ class AppAssets {
   static String homeIcon = 'assets/icons/home_icon.png';
   static String profileIcon = 'assets/icons/profile_icon.png';
   static String categoryIcon = 'assets/icons/category_icon.png';
+  static String cover1 = 'assets/images/cover_1.png';
+  static String cover2 = 'assets/images/cover_2.png';
+  static String collect2Image = 'assets/images/collect2_image.png';
+  static String collectImage = 'assets/images/collect_image.png';
+  static String plastic1Image = 'assets/images/plastic1_image.png';
+  static String recycleImage = 'assets/images/recycle_image.png';
+  static String recyclingImage = 'assets/images/recycling_image.png';
+  static String learnRecycle = 'assets/images/learn_recycle.png';
+  static String potelImage = 'assets/images/potel_image.png';
+  static String plasticImage = 'assets/images/plastic_image.png';
+  static String cartoonImage = 'assets/images/cartoon_image.png';
+  static String copperImage = 'assets/images/copper_image.png';
+  static String ironImage = 'assets/images/iron_image.png';
+  static String paperImage = 'assets/images/paper_image.png';
+  static String learnRecycleCover = 'assets/images/learn_recycle_cover.png';
+  static String plasticCover = 'assets/images/plastic_cover.png';
+  static String potelCover = 'assets/images/potel_cover.png';
 }
--- lib/features/ui/home/tabs/home_tab/home_tab.dart
+++ lib/features/ui/home/tabs/home_tab/home_tab.dart
@@ -1,12 +1,157 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:re_save_app/core/utils/app_assets.dart';
+import 'package:re_save_app/core/utils/app_styles.dart';
 
 import '../../../../../core/utils/app_colors.dart';
 
 class HomeTab extends StatelessWidget {
-  const HomeTab({super.key});
+  HomeTab({super.key});
+
+  List<Widget> asd = [
+    Image.asset(AppAssets.learnRecycleCover),
+    Image.asset(AppAssets.plasticCover),
+    Image.asset(AppAssets.potelCover),
+
+  ];
+  List<String> categoriesList = [
+    AppAssets.ironImage,
+    AppAssets.cartoonImage,
+    AppAssets.plasticImage,
+    AppAssets.copperImage,
+    AppAssets.paperImage,
+    AppAssets.plasticImage,
+    AppAssets.copperImage,
+    AppAssets.paperImage,
+  ];
+  List<String> categoriesNameList = [
+    'حديد',
+    'كرتون',
+    'بلاستيك',
+    'نحاس',
+    'ورق',
+    'بلاستيك',
+    'نحاس',
+    'ورق',
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: AppColors.yellowColor);
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(150.h),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+                horizontal: 20.w
+            ),
+            height: 150.h,
+            decoration: BoxDecoration(
+                color: AppColors.darkGreenColor,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(16.w),
+                  bottomLeft: Radius.circular(16.w),
+                )
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 50.h,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('ياسين إدريس', style: AppStyles.bold24White,),
+                        Text('المستوى 3', style: AppStyles.light16White,),
+                      ],
+                    ),
+                    Icon(
+                      Icons.notifications_outlined, color: AppColors.whiteColor,
+                      size: 30,)
+                  ],
+                ),
+              ],
+            ),
+          )
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: 20.w
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("العروض", style: AppStyles.medium18Header,),
+                  Text("عرض المزيد", style: AppStyles.bold16Blue,),
+                ],
+              ),
+              CarouselSlider(
+                  items: asd,
+                  options: CarouselOptions(
+                    padEnds: false,
+                    height: 200.h,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    scrollDirection: Axis.horizontal,
+                  )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("فئات", style: AppStyles.medium18Header,),
+                  Text("عرض المزيد", style: AppStyles.bold16Blue,),
+                ],
+              ),
+              SizedBox(height: 20.h,),
+              SizedBox(
+                height: 350.h,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.5,
+                    mainAxisSpacing: 20.h,
+                    //   crossAxisCount: 2,
+                    // childAspectRatio: 2,
+                    // mainAxisSpacing: 30.h,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(categoriesList[index]),
+                          radius: 50,
+                        ),
+                        SizedBox(height: 5.h,),
+                        Expanded(child: Text(categoriesNameList[index],
+                          style: AppStyles.bold20PrimaryDark,))
+                      ],
+                    );
+                  },
+                  itemCount: categoriesList.length,
+                  scrollDirection: Axis.horizontal,
+                ),
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
